7ee37de2fd7735a9cf96f91d8a5784027d71d7eb
AmirHossein Habibvand <ahhabibvand@gmail.com>
2023-07-16 10:37:32 +0330

    Remove float 16

--- src/stream.js
+++ src/stream.js
@@ -5,8 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.read = void 0;
 
-var _float = require("@petamoriken/float16");
-
 var _utils = require("./utils");
 
 // var _pickle = require("./pickle");
@@ -81,7 +79,6 @@ const TYPE_GETTERS_MAPPING = {
     '8': DataView.prototype.getBigInt64
   },
   f: {
-    '2': _float.getFloat16,
     '4': DataView.prototype.getFloat32,
     '8': DataView.prototype.getFloat64
   },
@@ -173,7 +170,7 @@ const readDType = (buf, dtype, shape) => {
       res.push(getterFn.apply(new DataView(Uint8Array.from(buf).buffer), [i * elSize, isLe, i, elSize]));
     }
   }
-  
+
 
   return (0, _utils.ndArray)(res, shape);
 };
@@ -217,36 +214,36 @@ const prep = async (rs
     } // Header comments are taken from
     // https://docs.scipy.org/doc/numpy-1.14.1/neps/npy-format.html#format-specification-version-1-0
     // The first 6 bytes are a magic string: exactly "x93NUMPY"
-  
-  
+
+
     const magicByteRaw = rs.read(1);
-  
+
     if (magicByteRaw === null) {
       resolve(null);
     } // $FlowFixMe
-  
-  
+
+
     const magicByte = magicByteRaw.readUInt8(); // offset += 1;
     // $FlowFixMe
-  
+
     const magicWord = rs.read(5).toString('ascii'); // offset += 5;
-  
+
     if (magicByte !== 0x93 || magicWord !== 'NUMPY') {
       throw new Error(`Unknown file type:\n  magic byte: ${magicByte}\n  magic word: "${magicWord}"`);
     } // The next 1 byte is an unsigned byte: the major version number of the file format, e.g. x01.
     // $FlowFixMe
-  
-  
+
+
     const versionMajor = rs.read(1).readUInt8(); // offset += 1;
     // The next 1 byte is an unsigned byte: the minor version number of the file format, e.g. x00.
     // $FlowFixMe
-  
+
     const versionMinor = rs.read(1).readUInt8(); // offset += 1;
     // Parse header length. This depends on the major file format version as follows:
-  
+
     (0, _utils.log)(`version: ${versionMajor}.${versionMinor}`, options);
     let headerLength;
-  
+
     if (versionMajor <= 1) {
       // The next 2 bytes form a little-endian unsigned short int:
       // the length of the header data HEADER_LEN.
@@ -258,31 +255,31 @@ const prep = async (rs
       // $FlowFixMe
       headerLength = rs.read(4).readUInt32LE(); // offset += 4;
     }
-  
+
     (0, _utils.log)(`header length: ${headerLength}`, options); // The next HEADER_LEN bytes form the header data describing the array’s format.
     // It is an ASCII string which contains a Python literal expression of a dictionary.
     // It is terminated by a newline (‘n’) and padded with spaces (‘x20’) to make the total
     // length of the magic string + 4 + HEADER_LEN be evenly divisible by 16.
-  
+
     const preludeLength = 6 + 4 + headerLength;
-  
+
     if (preludeLength % 16 !== 0) {
       process.stderr.write(`NPY file header is incorrectly padded. (${preludeLength} is not evenly divisible by 16.)`);
     } // $FlowFixMe
-  
-  
+
+
     const headerStr = rs.read(headerLength).toString('ascii');
     (0, _utils.log)(`header raw: "${headerStr}"`.replace('\n', '\\n').replace('\t', '\\t').replace('\r', '\\r').replace('"', '\\"'), options); // offset += headerLength;
-  
+
     const h = parseHeaderStr(headerStr);
     const dtype = h.descr;
     Object.keys(h).sort().forEach(k => (0, _utils.log)(`${k}: ${h[k]}`, options));
-  
+
     if (h.fortran_order) {
       throw new Error('NPY file is written in Fortran byte order, support for this byte order is not yet implemented.');
     } // Intepret the bytes according to the specified dtype
-  
-  
+
+
     const shape = Array.isArray(h.shape) && h.shape.length > 0 ? h.shape : [];
     resolve({h, shape})
   });
